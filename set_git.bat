b1bf32b5180a4025f17b4a08ccfe436f9809a9c0
Adan Butto <adone.1999@gmail.com>
2024-03-12 16:31:07 +0200

    first commit

---
+++ set_git.bat
@@ -0,0 +1,13 @@
+:: this bat file is used to set git repository
+:: to run use .\set_git.bat
+@echo off
+:: add :: before the next row to initialize the repository
+exit /b
+
+echo "# web_ex7" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/orbalmas/web_ex7.git
+git push -u origin main
